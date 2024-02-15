09735b43b8a2762aef9dd5ffdbc5f97bfbd457c1
subseonwoo <jooseonwoo6@gmail.com>
2024-02-15 14:30:43 +0900

    회원가입기능 만듬

--- client/src/components/CompanyJoin.jsx
+++ client/src/components/CompanyJoin.jsx
@@ -1,114 +1,131 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const CompanyJoin = () => {
-  const [businessNum, setBusinessNum] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [idInput, setIdInput] = useState('');
-  const [pwNum, setPwNum] = useState('');
-  const [idError, setIdError] = useState('');
-  const [pwError, setPwError] = useState('');
+    const [businessNum, setBusinessNum] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [idInput, setIdInput] = useState('');
+    const [pwNum, setPwNum] = useState('');
+    const [idError, setIdError] = useState('');
+    const [pwError, setPwError] = useState('');
 
-  const handleLogin = () => {
-    let hasError = false;
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/api/login', {
+                idInput: idInput,
+                pwNum: pwNum,
+                businessNum: businessNum
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error('ERROR!!', error);
+        }
+    };
 
-    // 사업자 등록 번호
-    if (!businessNum.trim()) {
-      setErrorMessage('올바른 사업자 등록 번호를 입력하세요.');
-    } else {
-      setErrorMessage('');
-      // 여기에 로그인 로직을 추가하세요
+    const handleLogin = () => {
+        let hasError = false;
+
+        // 사업자 등록 번호
+        if (!businessNum.trim()) {
+            setErrorMessage('올바른 사업자 등록 번호를 입력하세요.');
+        } else {
+            setErrorMessage('');
+            // 여기에 로그인 로직을 추가하세요
+        }
+        // 아이디
+        if (!idInput) {
+            setIdError('아이디를 입력해주세요.');
+        } else {
+            setIdError('');
+        }
+
+        // 비밀번호
+        if (!pwNum) {
+            setPwError('아이디를 입력해주세요.');
+        } else {
+            setPwError('');
+        }
+
+        // 로그인 처리
+        if (!hasError) {
+            // 여기에 로그인 처리 로직을 추가하세요
+
+
+            console.log('Logged in successfully!');
+        }
     }
-    // 아이디
-    if (!idInput) {
-      setIdError('아이디를 입력해주세요.');
-    } else {
-        setIdError('');
-    }
 
-    // 비밀번호
-    if (!pwNum) {
-        setPwError('아이디를 입력해주세요.');
-    } else {
-        setPwError('');
-    }
-
-    // 로그인 처리
-    if (!hasError) {
-      // 여기에 로그인 처리 로직을 추가하세요
-      console.log('Logged in successfully!');
-    }
-  }
-  
-  return (
-    <div className = "login-content3">
-      {/* Title Wrap*/}
-      <div className = "titleWrap3">
-          회원가입
-      </div>
-
-      {/* contentWrap - 아이디 입력 */}
-      <div className = "contentWrap3">
-            <div className="inputTitle3">
-                아이디
+    return (
+        <div className="login-content3">
+            {/* Title Wrap*/}
+            <div className="titleWrap3">
+                회원가입
             </div>
-            <div className="inputWrap3">
-                <input className="write-input3" type='text' placeholder='아이디'
-                value={idInput}
-                onChange={(e) => setIdInput(e.target.value)}></input>
-            </div>
+            <form onSubmit={handleLoginSubmit}>
+                {/* contentWrap - 아이디 입력 */}
+                <div className="contentWrap3">
+                    <div className="inputTitle3">
+                        아이디
+                    </div>
+                    <div className="inputWrap3">
+                        <input className="write-input3" type='text' placeholder='아이디'
+                            value={idInput}
+                            onChange={(e) => setIdInput(e.target.value)}></input>
+                    </div>
+                </div>
+
+                {/* errror 메세지 띄우기  */}
+                <div className="errorMessageWrap3">
+                    <div>{idError}</div>
+
+                </div>
+
+                {/* contentWrap - 비밀번호 입력 */}
+                <div className="contentWrap3">
+                    <div className="inputTitle3">
+                        비밀번호
+                    </div>
+                    <div className="inputWrap3">
+                        <input className="write-input3" type='password' placeholder='비밀번호'
+                            value={pwNum}
+                            onChange={(e) => setPwNum(e.target.value)}></input>
+                    </div>
+                </div>
+
+                {/* errror 메세지 띄우기  */}
+                <div className="errorMessageWrap3">
+                    <div>{pwError}</div>
+
+                </div>
+
+                {/* contentWrap - 사업자 등록 번호 입력 */}
+                <div className="contentWrap3">
+                    <div className="inputTitle3">
+                        사업자 등록 번호
+                    </div>
+                    <div className="inputWrap3">
+                        <input className="write-input3" placeholder='번호만'
+                            value={businessNum}
+                            onChange={(e) => setBusinessNum(e.target.value)}></input>
+                    </div>
+                </div>
+
+                {/* errror 메세지 띄우기  */}
+                <div className="errorMessageWrap3">
+                    <div>{errorMessage}</div>
+
+                </div>
+
+                {/* 로그인 버튼 */}
+                <div className='bottomBtn3'>
+                    <button className='loginBtn3'
+                        onClick={handleLogin}>Join</button>
+                </div>
+            </form>
         </div>
 
-        {/* errror 메세지 띄우기  */}
-        <div className = "errorMessageWrap3">
-            <div>{idError}</div>
-
-        </div>
-
-        {/* contentWrap - 비밀번호 입력 */}
-        <div className = "contentWrap3">
-            <div className="inputTitle3">
-                비밀번호
-            </div>
-            <div className="inputWrap3">
-                <input className="write-input3" type='password' placeholder='비밀번호'
-                value={pwNum}
-                onChange={(e) => setPwNum(e.target.value)}></input>
-            </div>
-        </div>
-
-        {/* errror 메세지 띄우기  */}
-        <div className = "errorMessageWrap3">
-            <div>{pwError}</div>
-
-        </div>
-
-      {/* contentWrap - 사업자 등록 번호 입력 */}
-      <div className = "contentWrap3">
-          <div className="inputTitle3">
-              사업자 등록 번호
-          </div>
-          <div className="inputWrap3">
-              <input className="write-input3" placeholder='번호만'
-              value={businessNum}
-              onChange={(e) => setBusinessNum(e.target.value)}></input>
-          </div>
-      </div>
-
-      {/* errror 메세지 띄우기  */}
-      <div className = "errorMessageWrap3">
-          <div>{errorMessage}</div>
-
-      </div>
-
-      {/* 로그인 버튼 */}
-      <div className='bottomBtn3'>
-            <button className='loginBtn3'
-            onClick={handleLogin}>Join</button>
-        </div>
-    
-    </div>
-    
-  )
+    )
 }
 
 export default CompanyJoin
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,45 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+ 
+from models import DatabaseHandler, moolLoan_user_table, getHashStr
+import hashlib
 
 app = Flask(__name__)
+CORS(app)
 
 # Members API Route
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+# @app.route("/members")
+# def members():
+#     return {"members": ["Member1", "Member2", "Member3"]}
+
+@app.route("/api/login", methods=['POST'])
+def login() :
+    data = request.json
+    idInput = data.get('idInput')
+    pwNum = data.get('pwNum')
+    businessNum = data.get('businessNum')
+    print(idInput, pwNum, businessNum)
+    pwNum = hashlib
+    
+    resultString = 'JoinAccess successful'
+    session = DatabaseHandler().session
+    hashPassword = getHashStr(pwNum)
+
+    try : 
+        session.begin()
+        newAccount = moolLoan_user_table(user_id=idInput, user_password=hashPassword, user_type='N')
+        session.add(newAccount)
+        session.commit()
+    except Exception as e :
+        resultString = 'ERROR!! : ' + e
+    finally :
+        added_rows = len(session.new)
+        session.close()
+    
+    if added_rows != 1 :
+        resultString = 'JoinAccess Failed'
+
+    return resultString
 
 if __name__ == "__main__":
     app.run(debug=True)
