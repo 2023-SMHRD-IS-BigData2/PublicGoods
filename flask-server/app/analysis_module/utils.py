d86554e506527a9d41d4652ddb3945aa74ed7220
Joohyoung Jeon <jeonjoohyoung@gmail.com>
2024-02-20 22:59:35 +0900

    [feat & chore] Add GrowthModel, modify gitignore and chore..

--- flask-server/app/analysis_module/utils.py
+++ flask-server/app/analysis_module/utils.py
@@ -1,7 +1,12 @@
+import os.path
+import datetime
 import pandas as pd
 import numpy as np
+import itertools
 from typing import List
+from typing import Union
 from sklearn.preprocessing import minmax_scale
+
 
 NEED_TO_CHECK_COLUMNS = ["ChgOffPrinGr", "GrAppv", "DisbursementGross", "BalanceGross", "SBA_Appv", "NAICS",
                          "MIS_Status"]
@@ -45,6 +50,160 @@ TARGET_YEARS = ["2018", "2019", "2020", "2021", "2022"]
 FIXED_INDICATORS = ["매출액", "영업이익", "영업이익(발표기준)", "당기순이익", "지배주주순이익", "비지배주주순이익", "자산총계",
                     "부채총계", "자본총계", "지배주주지분", "비지배주주지분", "자본금", "부채비율", "유보율", "영업이익률",
                     "지배주주순이익률", "PER", "EPS(원)", "PBR"]
+
+
+GROWTH_MODEL_FIXED_SECTORS = {
+    #  농업, 임업, 어업, 수렵, 목재
+    "11": ["한국:수출물가지수:농림수산품",
+           "한국:수출물가지수:농림수산품:계약통화",
+           "한국:수출물가지수:농림수산품:달러",
+           "한국:수출금액지수:농림수산품",
+           "한국:수출물량지수:농림수산품",
+           "한국:수출물량지수:농산물",
+           "한국:수출물량지수:수산물",
+           "한국:수출물가지수:목재",
+           "한국:수출금액지수:목재",
+           "한국:수출물량지수:목재",
+           "한국:수출물가지수:목재.1",
+           "한국:수출금액지수:농산물",
+           "한국:수출금액지수:수산물",
+           "한국:수출산업경기전망지수(EBSI):1차산업:농수산물",
+           ],
+    # 광업, 채석, 석유, 가스, 추출
+    "21": ["한국:수출물가지수:석탄",
+           "한국:수출금액지수:석탄",
+           "한국:수출물량지수:석탄",
+           "한국:수출물가지수:석탄.1",
+           "한국:수출물가지수:목재.2",
+           "한국:수출물가지수:석탄.2",
+           "한국:수출금액지수:비금속광물제품",
+           "한국:수출물량지수:비금속광물제품",
+           "한국:수출물가지수:비금속광물제품:원화",
+           "한국:수출물가지수:비금속광물제품:계약통화",
+           "한국:수출물가지수:비금속광물제품:달러",
+           "한국:수출산업경기전망지수(EBSI):중화학공업:석유제품"
+           ],
+    # 공익/공과 사업  (내수산업)
+    "22": [None],
+    # 건설 (내수산업)
+    "23": ["한국:수출물가지수:기계",
+           "한국:수출물가지수:기계.1",
+           "한국:수출물가지수:기계.2",
+           "한국:수출물가지수:기계.3",
+           "한국:수출물가지수:기계.4",
+           "한국:수출물가지수:기계.5",
+           "한국:수출물가지수:금속가공제품",
+           "한국:수출물가지수:금속가공제품.1",
+           "한국:수출물가지수:금속가공제품.2",
+           "한국:수출물가지수:금속가공제품.3",
+           "한국:수출물가지수:금속가공제품.4",
+           "한국:수출물가지수:금속가공제품.5",
+           "한국:수출금액지수:금속가공제품",
+           "한국:수출물량지수:금속가공제품",
+           "한국:수출물가지수:제1차금속제품:달러",
+           "한국:수출물가지수:제1차금속제품:계약통화",
+           "한국:수출산업경기전망지수(EBSI):중화학공업:기계류",
+           "한국:수출금액지수:기계",
+           "한국:수출물량지수:기계",
+           ],
+    # 제조
+    "31-33": ["한국:수출물가지수:섬유",
+              "한국:수출금액지수:섬유",
+              "한국:수출물량지수:섬유",
+              "한국:수출물가지수:섬유.1",
+              "한국:수출물가지수:섬유.2",
+              "한국:수출물가지수:화학제품:원화",
+              "한국:수출물가지수:화학제품:계약통화",
+              "한국:수출금액지수:화학제품",
+              "한국:수출물량지수:화학제품",
+              "한국:수출물가지수:화학제품:달러",
+              "한국:수출물가지수:제1차금속제품:원화",
+              "한국:수출금액지수:제1차금속제품",
+              "한국:수출물량지수:제1차금속제품",
+              "한국:수출물가지수:기타제조업제품:원화",
+              "한국:수출물가지수:기타제조업제품:계약통화",
+              "한국:수출물가지수:기타제조업제품:달러",
+              "한국:수출금액지수:기타제조업제품",
+              "한국:수출물량지수:기타제조업제품",
+              "한국:수출물가지수:공산품",
+              "한국:수출물가지수:공산품:계약통화",
+              "한국:수출금액지수:공산품",
+              "한국:수출물량지수:공산품",
+              "한국:수출산업경기전망지수(EBSI):경공업:섬유",
+              "한국:수출산업경기전망지수(EBSI):경공업:섬유.1",
+              "한국:수출산업경기전망지수(EBSI):경공업:소계",
+              "한국:수출산업경기전망지수(EBSI):경공업:플라스틱및고무제품",
+              "한국:수출산업경기전망지수(EBSI):중화학공업:화학공업",
+              "한국:수출산업경기전망지수(EBSI):중화학공업:철강",
+              "한국:수출산업경기전망지수(EBSI):중화학공업:선박",
+              "한국:수출산업경기전망지수(EBSI):중화학공업:자동차",
+              "한국:수출산업경기전망지수(EBSI):경공업:생활용품",
+              "한국:수출산업경기전망지수(EBSI):중화학공업:가전",
+              ],
+    # 도매 (내수산업)
+    "42": [None],
+    # 소매
+    "44-45": [None],
+    # 운송 및 창고
+    "48-49": ["한국:수출물가지수:운송장비",
+              "한국:수출물가지수:운송장비",
+              "한국:수출금액지수:운송장비",
+              "한국:수출물량지수:운송장비",
+              "한국:수출물가지수:운송장비.1",
+              "한국:수출물가지수:운송장비.2",
+              "한국:수출물가지수:운송장비.3",
+              "한국:수출물가지수:운송장비.4",
+              "한국:수출물가지수:운송장비.5"
+              ],
+    # 정보통신
+    "51": ["한국:수출물가지수:컴퓨터.2",
+           "한국:수출물가지수:컴퓨터.3",
+           "한국:수출물가지수:컴퓨터.4",
+           "한국:수출물가지수:컴퓨터.5",
+           "한국:수출물가지수:전기장비",
+           "한국:수출물가지수:전기장비",
+           "한국:수출물가지수:전기장비",
+           "한국:수출물가지수:전기장비.2",
+           "한국:수출물가지수:전기장비.4",
+           "한국:수출물가지수:전기장비.3",
+           "한국:수출물가지수:전기장비.5",
+           "한국:수출금액지수:전기장비",
+           "한국:수출물량지수:전기장비",
+           "한국:수출산업경기전망지수(EBSI):중화학공업:전기·전자제품",
+           "한국:수출산업경기전망지수(EBSI):중화학공업:무선통신기기"
+           ],
+    # 금융 (내수)
+    "52": [None],
+    # 부동산 (내수)
+    "53": [None],
+    # 전문 과학, 기술, 서비스
+    "54": ["한국:수출물가지수:컴퓨터",
+           "한국:수출금액지수:컴퓨터",
+           "한국:수출물량지수:컴퓨터",
+           "한국:수출물가지수:컴퓨터.1",
+           "한국:수출물가지수:전기장비.1",
+           "한국:수출산업경기전망지수(EBSI):중화학공업:반도체",
+           ],
+    # 회사 및 기업 경영관리
+    "55": [None],
+    # 행정 및 지원
+    "56": [None],
+    # 교육
+    "61": [None],
+    #  건강관리, 사실상 내수
+    "62": ["한국:수출산업경기전망지수(EBSI):중화학공업:의료·정밀"],
+    # 예술 및 엔터테인먼트
+    "71": [None],
+    "72": ["한국:수출물가지수:음식료품(원화",
+           "한국:수출물가지수:음식료품:계약통화",
+           "한국:수출금액지수:음식료품",
+           "한국:수출물량지수:음식료품",
+           "한국:수출물가지수:음식료품:달러"],
+    #  기타 서비스
+    "81": [None],
+    #  공공행정
+    "92": [None],
+}
 
 
 def convert_dollar_str_to_float(x: pd.Series) -> pd.Series:
@@ -97,6 +256,25 @@ def replace_inf_and_nan_with_smaller(row):
     row[row.isna()] = min_value - 1
 
     return row
+
+
+def create_date_index_dataframe(start: str, freq: str = "B", end: Union[str, None] = None) -> pd.DataFrame:
+    if end is None:
+        # end is none => end for "Now Date"
+        end = str(datetime.datetime.now().date())
+    else:
+        end = end
+    try:
+        pivot_date = pd.date_range(start=start, end=end, freq=freq)
+        pivot_date = pd.DataFrame(pivot_date)
+        pivot_date.columns = [freq]
+        pivot_date.set_index(freq, inplace=True)
+        pivot_date.index = pd.DatetimeIndex(pivot_date.index, freq=freq)
+        pivot_date.index = pivot_date.index.astype(str)
+        return pivot_date
+    except Exception as e:
+        print(e)
+        return pd.DataFrame()
 
 
 def preprocess_non_financial_data(data: pd.DataFrame) -> pd.DataFrame:
@@ -208,3 +386,84 @@ def preprocess_financial_data(data: pd.DataFrame) -> pd.DataFrame:
         left_index=True)
 
     return final_input_data
+
+
+# for Growth Model
+class InfomaxReader(object):
+    def __init__(self,
+                 skiprows=[0, 2, 3],
+                 header=0,
+                 date_name="DATE",
+                 date_format="%Y-%m-%d %H:%M:%S"):
+        self.skiprows = skiprows
+        self.header = header
+        self.date_name = date_name
+        self.date_format = date_format
+
+    def read_excel(self, path: str) -> pd.DataFrame:
+        data = pd.read_excel(path, dtype=str, skiprows=self.skiprows, header=self.header)
+        return data
+
+    def preprocess(self, data: pd.DataFrame) -> pd.DataFrame:
+        column_index = [[x, x + 1] for x in range(data.shape[1]) if x % 2 == 0]
+
+        sub_dfs = []
+        for idx in column_index:
+            sub_df = data.iloc[:, idx]
+            sub_df.columns = sub_df.columns.tolist()[::-1]
+            sub_df.columns = [self.date_name, sub_df.columns.tolist()[-1]]
+            sub_df = sub_df.set_index(sub_df.columns[0])
+            sub_df = sub_df.dropna()
+            sub_dfs.append(sub_df)
+
+        sub_dfs = pd.concat(sub_dfs, axis=1)
+        sub_dfs = sub_dfs.reset_index().sort_values(self.date_name).set_index(self.date_name)
+        sub_dfs.index = pd.to_datetime(sub_dfs.index, format=self.date_format).date.astype(str)
+        return sub_dfs
+
+    def read_prec(self, path: str) -> pd.DataFrame:
+        data = self.read_excel(path)
+        return self.preprocess(data)
+
+
+def preprocessing_growth_model_data(data_path: str, sector_code: str) -> pd.DataFrame:
+    if not os.path.isfile(data_path):
+        raise Exception("File does not exist: {}".format(data_path))
+
+    if sector_code not in GROWTH_MODEL_FIXED_SECTORS.keys():
+        raise Exception("Input sector code is not valid: {}".format(sector_code))
+
+    reader = InfomaxReader(skiprows=[0, 1, 3])
+
+    export_data = reader.read_prec(data_path)
+
+    # ** important **
+    korean_target_col_names = GROWTH_MODEL_FIXED_SECTORS[sector_code]
+    sub_df = export_data[korean_target_col_names].astype("float").copy()
+    freq = "BME"  # Business Monthly End, Freq "BME" was supported by pandas >= 2.2.0
+
+    pivot_date = create_date_index_dataframe(start="1996-01-31", freq=freq)
+
+    sub_df_monthly = pd.merge(
+        pivot_date, sub_df, how="left", left_index=True, right_index=True).interpolate()
+
+    drop_cols = sub_df_monthly.columns[sub_df_monthly.isna().sum() > 0]
+    sub_df_monthly = sub_df_monthly.drop(columns=drop_cols)
+
+    price_cols = [col_name for col_name in sub_df_monthly.columns.tolist() if str(col_name).__contains__("물가")]
+    quantity_cols = [col_name for col_name in sub_df_monthly.columns.tolist() if str(col_name).__contains__("물량")]
+
+    price_df = sub_df_monthly[price_cols].mean(axis=1)
+    quantity_df = sub_df_monthly[quantity_cols].mean(axis=1)
+
+    rtn_df = ((price_df * quantity_df).pct_change() * 1.0).dropna()
+    rtn_df = rtn_df.to_frame()
+    rtn_df.columns = ["values"]
+    rtn_df.index = pd.DatetimeIndex(rtn_df.index, freq=freq)
+    return rtn_df
+
+
+
+
+
+
