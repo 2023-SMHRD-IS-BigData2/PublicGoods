12a2b87796b1f9bcacdf561dfd4f6feea4a8616b
LimDai <oim01084420175@gmail.com>
2024-02-22 08:59:51 +0900

    [fix]조회 페이지, css 수정

--- client/src/components/ShowPage.jsx
+++ client/src/components/ShowPage.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ShowPage = () => {
+
+  const [placeholder, setPlaceHolder] = useState('사원번호');
+
+  const handleInputClick = () => {
+    setPlaceHolder('');
+  }
+
   return (
     <div className='onePage-div'>
       <div className='searchBox'>
-        <input></input>
+        <input className='searchInput' type='text' placeholder={placeholder} onClick={handleInputClick}></input>
+        <button className='searchBtn'>조회</button>
       </div>
     </div>
   )
