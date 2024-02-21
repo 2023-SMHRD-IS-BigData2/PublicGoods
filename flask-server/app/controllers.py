47f1d5bcc0d06c93c837c3a99d88f9219bfe1674
subseonwoo <jooseonwoo6@gmail.com>
2024-02-21 16:19:21 +0900

    [feat] 프론트에서 받은 데이터 전치리 class 추가, (시험작동 성공)

---
+++ flask-server/app/controllers.py
@@ -0,0 +1,51 @@
+from analysis_module.non_financial_model import NonFinancialModel
+from analysis_module.financial_model import FinancialModel
+from typing import Union, Tuple
+import pandas as pd
+import numpy as np
+import json
+
+class NonFinJsonToDict :
+    def __init__(self, AnyFinJson : Union[str, dict]) -> None :
+        if isinstance(AnyFinJson, str) : self.AnyFinDict = json.loads(AnyFinJson)
+        elif isinstance(AnyFinJson, dict) : self.AnyFinDict = AnyFinJson
+    
+        self.NonFinNewKey = ['연체여부', '대출청산여부', '대출보유기간(월)', '계열사여부', '보증금액(만원)', '수도권여부', '고용인원수', '대출금액']
+
+        self.FinNewKey = ['매출액', '영업이익', '영업이익(발표기준)', '당기순이익', '지배주주순이익', '비지배주주순이익', '자산총계', 
+                          '부채총계', '자본총계', '지배주주지분', '비지배주주지분', '자본금', '부채비율', '유보율', '영업이익률',
+                          '지배주주순이익률', 'PER', 'EPS(원)', 'PBR', 'Sector_Code']
+        
+        self.SimpleFinNewKey = ['', '', '', '', '']
+
+    def changeNonFinKey(self) -> dict :
+        if len(self.AnyFinDict) != 8 : return None
+        NonFinOldKey = list(self.AnyFinDict.keys())
+
+        for old, new in zip(NonFinOldKey, self.NonFinNewKey) :
+            self.AnyFinDict[new] = self.AnyFinDict.pop(old)
+
+        return self.AnyFinDict
+    
+    def changeFinKey(self) -> dict :
+        if len(self.AnyFinDict) != 20 : return None
+        FinOldKey = list(self.AnyFinDict.keys())
+
+        for old, new in zip(FinOldKey, self.FinNewKey) :
+            self.AnyFinDict[new] = self.AnyFinDict.pop(old)
+        
+        return self.AnyFinDict
+    
+    def runningNonFinModel(self) -> np.ndarray :
+        data = pd.DataFrame([self.AnyFinDict])
+        model = NonFinancialModel.load('파일경로')
+        predictions = model.predict(data, pred_prob=False)
+
+        return predictions
+
+    def runningFinModel(self) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]] :
+        data = pd.DataFrame([self.AnyFinDict])
+        model = FinancialModel.load('./analysis_module/FinModel/')
+        predictions = model.predict(data, pred_prob=False)
+
+        return predictions
