38252256f89ae5f69a0a556e688e0865076bb281
LimDai <oim01084420175@gmail.com>
2024-02-23 19:22:22 +0900

    [fix]로그인, 회원가입 수정

--- client/src/components/BankJoin.jsx
+++ client/src/components/BankJoin.jsx
@@ -2,15 +2,12 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
 
-const Join = () => {
+const BankLogin = () => {
+
     const [bankName, setBankName] = useState('');
     const [bankNumber, setBankNumber] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [NumErrorMessage, setNumErrorMessage] = useState('');
     const [idInput, setIdInput] = useState('');
     const [pwNum, setPwNum] = useState('');
-    const [idError, setIdError] = useState('');
-    const [pwError, setPwError] = useState('');
 
     const navigate = useNavigate();
 
@@ -18,33 +15,11 @@ const Join = () => {
         let hasError = false;
 
         // 은행명 유효성 검사
-        if (!bankName.trim()) {
-            setErrorMessage('올바른 은행명을 입력해주세요.');
+        if (!bankName.trim() || !bankNumber.trim() || !idInput || !pwNum) {
+            alert('올바르게 입력해주세요.');
             hasError = true;
         } else {
-            setErrorMessage('');
-        }
-
-        // 사원번호 유효성 검사
-        if (!bankNumber.trim()) {
-            setNumErrorMessage('올바른 사원번호를 입력해주세요.');
-            hasError = true;
-        } else {
-            setNumErrorMessage('');
-        }
-
-        // 아이디
-        if (!idInput) {
-            setIdError('아이디를 입력해주세요.');
-        } else {
-            setIdError('');
-        }
-
-        // 비밀번호
-        if (!pwNum) {
-            setPwError('아이디를 입력해주세요.');
-        } else {
-            setPwError('');
+            
         }
 
         // 로그인 처리
@@ -72,94 +47,66 @@ const Join = () => {
         }
     };
 
-    return (
-        <div className="login-content2">
-            {/* Title Wrap*/}
-            <div className="titleWrap2">
-                회원가입
-            </div>
-            {/* contentWrap - 아이디 입력 */}
-            <div className="contentWrap2">
-                <div className="inputTitle2">
-                    아이디
+  return (
+    <div className='loginPage'>
+
+        <div className='joinBox'>
+            {/* 로그인 설명 */}
+            <div className='joinInfo'>
+            <div className='join'>
+                <h3 className='LOC'>Join Our Community</h3>
+                <div className='inputBox'>
+                    {/* 로그인 제목 */}
+                    <h2 className='joinSub'>회원가입</h2>
+                    {/* input */}
+                    <div className='inputIdBox'>
+                        {/* 아이디 */}
+                        <input type="text" className='idInputJoin' placeholder='아이디'
+                               value={idInput}
+                               onChange={(e) => setIdInput(e.target.value)}
+                        />
+                    </div>
+
+                    {/* 비밀번호 */}
+                    <div className='inputPwBox'>
+                        <input type="text" className='pwInputJoin' placeholder='비밀번호'
+                               value={pwNum}
+                               onChange={(e) => setPwNum(e.target.value)}
+                        />
+                    </div>
+
+                    {/* 은행명 */}
+                    <div className='inputPwBox'>
+                        <input type="text" className='pwInputJoin' placeholder='예) 기업은행,SC제일은행(띄어쓰기 X)'
+                               value={bankName}
+                               onChange={(e) => setBankName(e.target.value)}
+                        />
+                    </div>
+
+                    {/* 사원번호 */}
+                    <div className='inputCompanyBox'>
+                        <input type="text" className='nbInputJoin' placeholder='사원 번호'
+                               value={bankNumber}
+                               onChange={(e) => setBankNumber(e.target.value)}
+                        />
+                    </div>
                 </div>
-                <div className="inputWrap2">
-                    <input className="write-input2" type='text' placeholder='아이디'
-                        value={idInput}
-                        onChange={(e) => setIdInput(e.target.value)}></input>
-                </div>
-            </div>
 
-            {/* errror 메세지 띄우기  */}
-            <div className="errorMessageWrap2">
-                <div>{idError}</div>
-
-            </div>
-
-            {/* contentWrap - 비밀번호 입력 */}
-            <div className="contentWrap2">
-                <div className="inputTitle2">
-                    비밀번호
-                </div>
-                <div className="inputWrap2">
-                    <input className="write-input2" type='password' placeholder='비밀번호'
-                        value={pwNum}
-                        onChange={(e) => setPwNum(e.target.value)}></input>
-                </div>
-            </div>
-
-            {/* errror 메세지 띄우기  */}
-            <div className="errorMessageWrap2">
-                <div>{pwError}</div>
-
-            </div>
-
-            {/* contentWrap - 은행명 입력 */}
-            <div className="contentWrap2">
-                <div className="inputTitle2">
-                    은행명(띄어쓰기 없이)
-                </div>
-                <div className="inputWrap2">
-                    <input className="write-input2" type='text' placeholder='예) 기업은행, SC제일은행, ...'
-                        value={bankName}
-                        onChange={(e) => setBankName(e.target.value)}></input>
+                {/* 로그인 버튼 */}
+                <div className='BtnBox'>
+                    <button className='logBtn2' onClick={handleLogin}>회원가입</button>
                 </div>
             </div>
-
-            {/* errror 메세지 띄우기  */}
-            <div className="errorMessageWrap2">
-                <div>{errorMessage}</div>
-
             </div>
 
-            {/* contentWrap - 사원번호 입력 */}
-            <div className="contentWrap2">
-                <div className="inputTitle2">
-                    사원번호
-                </div>
-                <div className="inputWrap2">
-                    <input className="write-input2" type='text' placeholder='번호만'
-                        value={bankNumber}
-                        onChange={(e) => setBankNumber(e.target.value)}></input>
-                </div>
-            </div>
-
-            {/* errror 메세지 띄우기  */}
-            <div className="errorMessageWrap2">
-                <div>{NumErrorMessage}</div>
-
-            </div>
-
-            {/* 로그인 버튼 */}
-            <div className='bottomBtn2'>
-            <button className='loginBtn2'
-                onClick={handleLogin}
-                style={{marginBottom : '50px'}}
-                >Join
-            </button>
+            {/* 로그인 */}
+            <div className='joinInfo'>
+                <h3 className='LOC'>Join Our Community</h3>
             </div>
         </div>
-    )
+
+    </div>
+  )
 }
 
-export default Join
+export default BankLogin
